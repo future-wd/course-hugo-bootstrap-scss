71c632c3125a7eeddcb05f93225cf9262787f5ad
sean-pwd <sean@powerfulwebdesign.com.au>
2021-12-06 19:29:11 +1100

    lesson 10 complete

--- postcss.config.js
+++ postcss.config.js
@@ -2,9 +2,8 @@ const autoprefixer = require('autoprefixer');
 
 module.exports = {
   plugins: [
-    // these plugins below will not be run during development
-    !process.env.HUGO_ENVIRONMENT === 'development'
-    ? autoprefixer()
-    : null
+    ...process.env.HUGO_ENVIRONMENT === 'development'
+    ? [null]
+    : [autoprefixer()]
   ]
 }
