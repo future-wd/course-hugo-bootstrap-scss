6df2a3740d2a9acbb7c7e95cd9f814af6ecd4c20
sean-pwd <sean@powerfulwebdesign.com.au>
2021-12-06 22:24:02 +1100

    lesson 11 complete

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,26 @@
-const autoprefixer = require('autoprefixer');
+const autoprefixer = require('autoprefixer')();
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: [
+    './layouts/**/*.html',
+    './content/**/*.md'
+  ],
+  safelist: {
+    // standard: [],
+    // deep: [],
+    greedy: [
+      /^pagination/,
+      /^page-/
+    ]
+  }
+})
+
 
 module.exports = {
   plugins: [
     ...process.env.HUGO_ENVIRONMENT === 'development'
-    ? [null]
-    : [autoprefixer()]
+      ? [null]
+      : [autoprefixer,
+        purgecss
+      ]
   ]
 }
